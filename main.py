5ec6e2788704fdf7c0532b4dfb9f021c614758ab
Вера <vera.vinichenki30.04@gmail.com>
2025-12-10 13:09:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,16 +18,16 @@ fam = [st for st in data if re.fullmatch(r'[A-Z][a-zA-Z]+', st)]
 emails = [st for st in data if re.fullmatch(r'\S+@\S+', st)] 
 date = [st for st in data if re.fullmatch(r'\d{4}-\d{2}-\d{2}', st)]
 web = [st for st in data if re.fullmatch(r'https?://\S+|www\.\S+', st)] 
+
 rows = [[ids[i], fam[i], emails[i], date[i], web[i]] for i in range(len(ids))]
 
 if __name__ == '__main__':
-    print(re.findall(r'\bс[а-яё]+\b', text, flags=re.I))  
-    print(re.findall(r'\bи\s+([а-яё]+)\b', text, flags=re.I))  
+    print(f'\n {re.findall(r'\bс[а-яё]+\b', text, flags=re.I)}')  
+    print(f'\n {re.findall(r'\bи\s+([а-яё]+)\b', text, flags=re.I)}')  
 
-    print(re.findall(r"font-family\s*:\s*'([^']+)'", html)) 
+    print(f'\n {re.findall(r"font-family\s*:\s*'([^']+)'", html)}') 
 
     with open('task3_solving.csv','w', encoding='utf-8', newline='') as f:
         table = csv.writer(f)
         table.writerow(['ID','Фамилия','Email','Дата','Сайт'])
-
         table.writerows(rows)
